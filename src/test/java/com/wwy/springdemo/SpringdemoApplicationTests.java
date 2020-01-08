ff4030ea57bb0c55435a8c327ade3a0b5f2ec229
魏文尧 <1029215431@qq.com>
2020-01-08 11:40:11 +0800

    测试

---
+++ src/test/java/com/wwy/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwy.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
